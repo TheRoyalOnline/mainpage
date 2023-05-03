11792ddc62547b1f2bb19fa5251f36aabac596a8
Diego Arrua <130692624+TheRoyalOnline@users.noreply.github.com>
2023-05-03 11:49:57 -0400

    intento

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 
 
 import MainPage from './Slots';
-import Signup from './Signup';
+import Signup, { SignUp } from './Signup';
 import Navbar from './Navbar';
 
 export default function App() {
     return (
         <div className='containter-fluid'>
-
             <Navbar />
             <Routes>
-                <Route exact path='/' Component={MainPage} />
-                <Route exact path='/Singup' Component={Signup} />
+                <Route exact path='/' element={<MainPage/>} />
+                <Route exact path='/Singup' element={<SignUp/>} />
             </Routes>
-
-
         </div>
     );
 }
